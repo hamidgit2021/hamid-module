314e5e972d0f353ffe5da8fda406c97724c417cd
hamidgit2021 <79046793+hamidgit2021@users.noreply.github.com>
2024-01-09 20:24:42 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 output "subnet1_id" {
-  value = aws_subnet.pub_one.id
+  value = aws_subnet.pub_one_cidr.id
 }
 
 output "subnet2_id" {
-  value = aws_subnet.pub_two.id
+  value = aws_subnet.pub_two_cidr.id
 }
 
 output "subnet3_id" {
-  value = aws_subnet.priv_one.id
+  value = aws_subnet.priv_one_cidr.id
 }
 
 output "subnet4_id" {
-  value = aws_subnet.priv_two.id
+  value = aws_subnet.priv_two_cidr.id
 }
 
